782fecb7e04cf8d88fcb41de5d18b54330ddec1b
IFirestarteRI <138885722+IFirestarteRI@users.noreply.github.com>
2023-07-11 17:10:40 +0300

    Add files via upload

---
+++ razor/BlazorApp1/BlazorApp1/DTO/GeoObjectTypeDtoMapper.cs
@@ -0,0 +1,47 @@
+﻿using BlazorApp1.Models ;
+
+namespace BlazorApp1.Models
+{
+    public static class GeoObjectTypeDtoMapper
+    {
+        public static GeoObjectTypeDto ToDto(GeoObjectType geoObjectType) => new GeoObjectTypeDto
+        {
+            Code = geoObjectType.Code,
+            TypeSectionCode = geoObjectType.TypeSectionCode,
+            NameInEnglish = geoObjectType.NameInEnglish,
+            NameInRussian = geoObjectType.NameInRussian,
+            DescriptionInEnglish = geoObjectType.DescriptionInEnglish,
+            DescriptionInRussian = geoObjectType.DescriptionInRussian
+        };
+
+        public static GeoObjectType ToEntity(GeoObjectTypeDto geoObjectTypeDto) => new GeoObjectType
+        {
+            Code = geoObjectTypeDto.Code,
+            TypeSectionCode = geoObjectTypeDto.TypeSectionCode,
+            NameInEnglish = geoObjectTypeDto.NameInEnglish,
+            NameInRussian = geoObjectTypeDto.NameInRussian,
+            DescriptionInEnglish = geoObjectTypeDto.DescriptionInEnglish,
+            DescriptionInRussian = geoObjectTypeDto.DescriptionInRussian
+        };
+
+        public static List<GeoObjectTypeDto> ToDto(List<GeoObjectType> geoObjectTypes)
+        {
+            var geoObjectTypeDtos = new List<GeoObjectTypeDto>();
+            foreach (GeoObjectType geoObjectType in geoObjectTypes)
+            {
+                geoObjectTypeDtos.Add(ToDto(geoObjectType));
+            }
+            return geoObjectTypeDtos;
+        }
+
+        public static List<GeoObjectType> ToEntity(List<GeoObjectTypeDto> geoObjectTypeDtos)
+        {
+            var entityTypes = new List<GeoObjectType>();
+            foreach(var geoObjectTypeDto in geoObjectTypeDtos)
+            {
+                entityTypes.Add(ToEntity(geoObjectTypeDto));
+            }
+            return entityTypes;
+        }
+    }
+}
---
+++ razor/BlazorApp1/BlazorApp1/Models/GeoObjectType.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorApp1.Models
+{
+    public class GeoObjectType
+    {
+        public string Code { get; set; } = String.Empty;
+        public string NameInEnglish { get; set; } = String.Empty;
+        public string NameInRussian { get; set; } = String.Empty;
+        public string DescriptionInEnglish { get; set; } = String.Empty;
+        public string DescriptionInRussian { get; set; } = String.Empty;
+        public string TypeSectionCode { get; set; } = String.Empty;
+        
+    }
+}
